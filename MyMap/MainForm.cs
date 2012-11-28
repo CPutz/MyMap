af962b02b54d601d87af9285c6663f74935a73a2
Casper_Desktop <css_mn@yahoo.ie>
2012-11-28 16:50:02 +0100

    MainForm aangemaakt ipv zo'n designer Form

---
+++ MyMap/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyMap
+{
+    class MainForm : Form
+    {
+        public MainForm()
+        {
+        }
+    }
+}
--- MyMap/Program.cs
+++ MyMap/Program.cs
@@ -15,7 +15,7 @@ namespace MyMap
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
