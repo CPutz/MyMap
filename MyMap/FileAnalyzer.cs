a78c7008e233ccb3efd8cb530290e3d698bf1d05
Sophie Huiberts <itissohardtothinkofagoodemail@gmail.com>
2012-12-01 19:59:25 +0100

    stats setup

--- MyMap/FileAnalyzer.cs
+++ MyMap/FileAnalyzer.cs
@@ -12,27 +12,28 @@ namespace MyMap
     public class FileAnalyzer : Form
     {
         // In whole coordinates
-        double leftbound = -3;
+        /*double leftbound = -3;
         double rightbound = 9;
         double upperbound = 59;
-        double lowerbound = 49;
+        double lowerbound = 49;*/
 
         int renderwidth = 2000;
         int renderheight = 2000;
 
         public FileAnalyzer(string path)
         {
-            Bitmap bm = analyzeFile(path);
-            bm.Save("output.png");
+            analyzeFile(path);
             Application.Exit();
         }
 
         // Primary documentation is in http://wiki.openstreetmap.org/wiki/PBF_Format
         // Feel free to ask me anything you don't understand of course
-        Bitmap analyzeFile(string s) {
-            FileStream f = new FileStream(s, FileMode.Open);
+        void analyzeFile(string s) {
+            long previousid = 0;
 
-            long[,] counter = new long[renderwidth, renderheight];
+            //long[,] counter = new long[renderwidth, renderheight];
+
+            FileStream f = new FileStream(s, FileMode.Open);
 
             double nodes = 0;
             while(true) {
@@ -88,59 +89,51 @@ namespace MyMap
 
                 } else if(blobHead.Type == "OSMData")
                 {
-                    Console.WriteLine("-------------------------------------------------");
-                    Console.WriteLine("Block data info");
 
                     PrimitiveBlock pb = PrimitiveBlock.ParseFrom(blockData);
 
                     for(int i = 0; i < pb.PrimitivegroupCount; i++)
                     {
                         PrimitiveGroup pg = pb.GetPrimitivegroup(i);
-                        Console.WriteLine("primitivegroup " + i + " " +
-                        pg.ChangesetsCount + " " +
-                        pg.NodesCount + " " +
-                        pg.RelationsCount + " " +
-                        pg.WaysCount + " " +
-                        pg.HasDense);
                         if(pg.HasDense)
                         {
                             DenseNodes dense = pg.Dense;
-                            Console.WriteLine("densenodes " +
-                                              dense.LatCount + " " +
-                                              dense.LonCount + " " +
-                                              dense.KeysValsCount + " " +
-                                              dense.IdCount);
 
                             double lat = 0, lon = 0;
                             long id = 0;
                             for(int j = 0; j < dense.LatCount; j++)
                             {
-                                bool[,] gotit = new bool[renderwidth, renderheight];
                                 long dlat = dense.GetLat(j);
                                 long dlon = dense.GetLon(j);
                                 double dlatitude = .000000001 * (pb.LatOffset + (pb.Granularity * dlat));
                                 double dlongitude = .000000001 * (pb.LonOffset + (pb.Granularity * dlon));
-                                Console.WriteLine("coordinates " +
-                                                  (lat += dlatitude) + " " +
-                                                  (lon += dlongitude) + " " +
-                                                  (id += dense.GetId(j)) + " " +
-                                                  dense.GetKeysVals(j));
-                                if(!gotit[(int)((lon - leftbound)/(rightbound - leftbound)*renderwidth),
-                                      (int)((upperbound - lat)/(upperbound - lowerbound)*renderheight)])
-                                    counter[(int)((lon - leftbound)/(rightbound - leftbound)*renderwidth),
-                                            (int)((upperbound - lat)/(upperbound - lowerbound)*renderheight)]++;
+                                lat += dlatitude;
+                                lon += dlongitude;
+                                id += dense.GetId(j);
 
-                                gotit[(int)((lon - leftbound)/(rightbound - leftbound)*renderwidth),
-                                      (int)((upperbound - lat)/(upperbound - lowerbound)*renderheight)]
-                                = true;
+                                if(id > previousid)
+                                    previousid = id;
+                                else if(id == previousid)
+                                    Console.WriteLine("Same id at " + id);
+                                else
+                                    Console.WriteLine("Lower id: " + previousid + " to " + id);
 
                                 nodes++;
                             }
                         } else {
-                            /*for(int j = 0; j < pg.NodesCount; j++)
+                            for(int j = 0; j < pg.NodesCount; j++)
                             {
-                                pg.GetNodes(j)
-                            }*/
+                                nodes++;
+                                long id = pg.GetNodes(j).Id;
+
+                                if(id > previousid)
+                                    previousid = id;
+                                else if(id == previousid)
+                                    Console.WriteLine("Same id at " + id);
+                                else
+                                    Console.WriteLine("Lower id: " + previousid + " to " + id);
+
+                            }
                         }
                     }
                 } else
@@ -149,41 +142,6 @@ namespace MyMap
             }
             Console.WriteLine(nodes + " nodes found");
 
-            double minblocks = 1100000, maxblocks = 0, totalblocks = 0;
-
-            for(int i = 0; i < renderwidth; i++) {
-                for(int j = 0; j < renderheight; j++) {
-
-                    minblocks = Math.Min (minblocks, counter[i, j]);
-                    maxblocks = Math.Max(maxblocks, counter[i, j]);
-                    totalblocks += counter[i, j];
-                }
-            }
-
-            double avgblocks = totalblocks/(renderwidth*renderheight);
-            Bitmap bm = new Bitmap(renderwidth, renderheight);
-
-            for(int i = 0; i < renderwidth; i++) {
-                for(int j = 0; j < renderheight; j++) {
-                    int greyness;
-
-                    // average blocks
-                    if(counter[i, j] >= avgblocks) {
-                        greyness = (int)(128 + 127*(counter[i, j]-avgblocks)/(maxblocks-avgblocks));
-                    } else {
-                        greyness = (int)(127*(counter[i,j]-minblocks)/(avgblocks-minblocks));
-                    }
-
-                    bm.SetPixel(i, j, Color.FromArgb(greyness, greyness, greyness));
-                }
-            }
-            
-            Console.WriteLine("");
-            Console.WriteLine("minblocks:" + minblocks);
-            Console.WriteLine("avgblocks:" + avgblocks);
-            Console.WriteLine("maxblocks:" + maxblocks);
-
-            return bm;
         }
 
         static byte[] zlibdecompress(byte[] compressed)
