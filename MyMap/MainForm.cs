6440e858d70365c3bf7ac306670d867115a47871
Chiel desktop <griec@live.nl>
2013-01-12 13:08:07 +0100

    bij nader inzien, geen idee waarom ik daar het bestand moet lezen, dus verwijderd

--- MyMap/MainForm.cs
+++ MyMap/MainForm.cs
@@ -511,14 +511,13 @@ namespace MyMap
             List<string> woorden = new List<string>();
             int n = 0;
             char[] separators = { ',' };
-            StreamReader sr= null;
+
             
             menu.DropDownItems.Add("Save", null, this.Save);
             menu.DropDownItems.Add("verander gebruiker", null, this.VeranderGebruiker);
             
             ToolStripMenuItem verwijdersubmenu = new ToolStripMenuItem("verwijdergebuiker");
-            try {  sr = new StreamReader("gebruikers.txt"); }
-            catch { }
+
 
                 foreach (string g in userData)
                 {
@@ -537,11 +536,6 @@ namespace MyMap
 
             if (areNewUsers)
             menu.DropDownItems.Add(verwijdersubmenu);
-                
-            try { sr.Close(); }
-            catch { }
-
-            
 
             menuStrip.Items.Add(menu);
             this.Controls.Add(menuStrip);
