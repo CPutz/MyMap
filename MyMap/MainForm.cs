6ff71363636fcb85763f6f8a2790e999c9bc11fb
Casper_Desktop <css_mn@yahoo.ie>
2013-01-23 11:36:50 +0100

    selectbox heeft tooltip

--- MyMap/MainForm.cs
+++ MyMap/MainForm.cs
@@ -82,7 +82,10 @@ namespace MyMap
                     toolTipCar = new ToolTip(), 
                     toolTipCheckBike = new ToolTip(), 
                     toolTipCheckCar = new ToolTip(), 
-                    toolTipCheckPT = new ToolTip(); 
+                    toolTipCheckPT = new ToolTip(),
+                    toolTipStartBox = new ToolTip(),
+                    toolTipViaBox = new ToolTip(),
+                    toolTipEndBox = new ToolTip();
             
 
             map = new MapDisplay(10, 110, this.ClientSize.Width - 20, this.ClientSize.Height - 120, loadingThread);
@@ -114,14 +117,17 @@ namespace MyMap
 
             fromBox.Location = new Point(100, 8);
             fromBox.Size = new Size(200, 30);
+            toolTipStartBox.SetToolTip(fromBox, "Search for streets, press Enter to place from icon.");
             this.Controls.Add(fromBox);
 
             viaBox.Location = new Point(100, 38);
             viaBox.Size = new Size(200, 30);
+            toolTipStartBox.SetToolTip(fromBox, "Search for streets, press Enter to place via icon.");
             this.Controls.Add(viaBox);
 
             toBox.Location = new Point(100, 68);
             toBox.Size = new Size(200, 30);
+            toolTipStartBox.SetToolTip(fromBox, "Search for streets, press Enter to place to icon.");
             this.Controls.Add(toBox);
 
 
